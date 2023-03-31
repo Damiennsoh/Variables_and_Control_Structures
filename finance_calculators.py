2fc86879a33f1f8320e5433f829589ab0717ae5f
Damiennsoh <psycaro20@gmail.com>
2023-04-01 01:08:58 +0530

    converted string inputs into ingers

--- finance_calculators.py
+++ finance_calculators.py
@@ -8,10 +8,23 @@ print(bond)
 
 calculation_type_Choice = input("Enter either 'investment' or 'bond' from the menu above to proceed: ").lower() # converts input to lowercase.
 if calculation_type_Choice == "investment":
-    deposit_amount = input("What amount are you depositing: ")
+    deposit_amount = int(input("What amount are you depositing: ")) # int is placed in front of the  input before the parenthesis to convert any string into an integer
     print("Deposit amount is:", deposit_amount)
-    interest_Rate = input("What interest rate do you want?: ")
+   
+    interest_Rate = int(input("What interest rate do you want?: ")) 
     print("Interest rate is:", interest_Rate)
+   
+    investment_years = input("How many years do you plan on investing?: ")
+    print("Investment years is:", investment_years)
+
+# Declaring variables for both simple interest and compound interest and their respective formulas
+simple_interest = (deposit_amount*interest_Rate*investment_years)*100
+total_simple_interest_amount = deposit_amount + simple_interest # Adding the simple interest amount to the principal or initial amount invested
+
+interest_type = input("Choose between simple interest or compound interest").lower() # Declaring a variable for type of interest
+if interest_type == "simple":
+    print("Simple interest =", simple_interest)
+    print("Total amount=",total_simple_interest_amount )
 
 elif calculation_type_Choice == "bond":
     print("Felicia will be fucked by me one day")

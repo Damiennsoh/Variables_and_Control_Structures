742902d3a314b19b0e9d9882881f6c5bd90cdb88
Damiennsoh <psycaro20@gmail.com>
2023-03-31 22:00:31 +0530

    modified the code in the python file

--- finance_calculators.py
+++ finance_calculators.py
@@ -8,7 +8,13 @@ print(bond)
 
 calculation_type_Choice = input("Enter either 'investment' or 'bond' from the menu above to proceed: ").lower() # converts input to lowercase.
 if calculation_type_Choice == "investment":
-    print("here i come")
+    deposit_amount = input("What amount are you depositing: ")
+    print("Deposit amount is:", deposit_amount)
+    interest_Rate = input("What interest rate do you want?: ")
+    print("Interest rate is:", interest_Rate)
+
+elif calculation_type_Choice == "bond":
+    print("Felicia will be fucked by me one day")
 elif len(calculation_type_Choice) < 1:
     print("You did not make a choice! please make a choice to proceed.")
 elif calculation_type_Choice != "investment" and calculation_type_Choice != "bond":
